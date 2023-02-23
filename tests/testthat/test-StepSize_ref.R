65f806864c9402a754085b98808746092eadd8ef
Dongdong Kong <kongdd@users.noreply.github.com>
2023-02-23 17:15:11 +0800

    minor update for testthat

--- tests/testthat/test-StepSize_ref.R
+++ tests/testthat/test-StepSize_ref.R
@@ -3,7 +3,7 @@ test_that("StepSize.wRef works", {
   Rseries <- system.file("extdata/Example2_Ref.dat", package = "RHtests")
 
   metadata <- data.table(date = c("1974-02-01", "1975-11-01"))
-  prefix <- "OUTPUT/example02/example02"
+  prefix <- "OUTPUT/example02"
 
   # r <- RHtests_process(Bseries, Rseries, metadata, prefix)
   # plot_RHtests(r)
@@ -15,9 +15,8 @@ test_that("StepSize.wRef works", {
 
   r <- RHtests_process(B, R, metadata, prefix)
   plot_RHtests(r)
-
+  
   # expect_equal(nrow(r$TP), 11)
-  expect_equal(as.character(r$TP[kind == 0]$date),
-               metadata$date)
+  expect_equal(as.character(r$TP[kind == 0]$date), metadata$date)
   expect_equal(nrow(r$data), 612)
 })
